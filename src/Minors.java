caed286e96f028165f0c18b6b9b0bcdc1a34c022
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-05-19 01:57:04 +0200

    add Efficient, Minors

--- src/Minors.java
+++ src/Minors.java
@@ -1,21 +1,21 @@
 import java.time.LocalDate;
 
 public class Minors extends Person {
-//    @Override
-//    public String toString() {
-//        return "Minors{" +
-//                "maximumSalary=" + maximumSalary +
-//                ", minimalSalary=" + minimalSalary +
-//                '}';
-//    }
-
-   public Minors(int minimalSalary, int maximumSalary) {
-       this.maximumSalary = maximumSalary;
-       this.minimalSalary = minimalSalary;
+    @Override
+    public String toString() {
+        return "Minors{" +
+                "maximumSalary=" + maximumSalary +
+                ", minimalSalary=" + minimalSalary +
+                '}';
     }
+
+//    public Minors(int minimalSalary, int maximumSalary) {
+//        this.maximumSalary = maximumSalary;
+//        this.minimalSalary = minimalSalary;
+//    }
 
     @Override
     double averageOfSalary() {
-        return MainFindingTheAverage.averageForTwo(maximumSalary,minimalSalary);
+        return MainFindingTheAverage.averageForTwo(maximumSalary, minimalSalary);
     }
 }
